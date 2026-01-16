5ca1d3b2ccec1c0a38c17fe02484b2b352135738
carloscncdev <carloscndev@gmail.com>
2026-01-15 19:58:02 -0600

    feat: update lint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,30 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
 });
 
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended", "plugin:prettier/recommended"),
-
+export default defineConfig([
+  js.configs.recommended, // ESLint
+  ...compat.extends("plugin:prettier/recommended"), // Prettier
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
     },
-
     rules: {
-        semi: ["error", "always"],
-        quotes: ["error", "double"],
-
-        "prettier/prettier": ["error", {
-            endOfLine: "auto",
-        }],
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
-}]);
+  },
+]);
