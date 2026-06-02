b664a1bcd44f39636cab642bae5f99919e770984
carloscncdev <carloscndev@gmail.com>
2026-06-02 13:46:39 -0600

    BACK-024: Create Single Type blog-page with title, intro, viewMore

---
+++ src/api/blog-page/content-types/blog-page/schema.ts
@@ -0,0 +1,49 @@
+import type { Schema } from '@strapi/strapi';
+
+export default {
+  kind: 'singleType',
+  collectionName: 'blog_pages',
+  info: {
+    singularName: 'blog-page',
+    pluralName: 'blog-pages',
+    displayName: 'Blog Page',
+    description: 'Blog section with title, intro, and view more label'
+  },
+  options: {
+    draftAndPublish: false
+  },
+  pluginOptions: {
+    i18n: {
+      localized: true
+    }
+  },
+  attributes: {
+    title: {
+      type: 'string',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    },
+    intro: {
+      type: 'text',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    },
+    viewMore: {
+      type: 'string',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    }
+  }
+} as const satisfies Schema.ContentType;
