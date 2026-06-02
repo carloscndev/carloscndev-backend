d4394ed9ae4a1b41259a8504b788cf888a2eea54
carloscncdev <carloscndev@gmail.com>
2026-06-01 20:07:03 -0600

    BACK-022: Create Collection Type blog-post with title, slug, resume, readTime, date, category relation, icon, headerImage, content, featuredImage

---
+++ src/api/blog-post/content-types/blog-post/schema.ts
@@ -0,0 +1,115 @@
+import type { Schema } from '@strapi/strapi';
+
+export default {
+  kind: 'collectionType',
+  collectionName: 'blog_posts',
+  info: {
+    singularName: 'blog-post',
+    pluralName: 'blog-posts',
+    displayName: 'Blog Post',
+    description: 'Blog post entries'
+  },
+  options: {
+    draftAndPublish: false
+  },
+  pluginOptions: {
+    i18n: {
+      localized: true
+    }
+  },
+  attributes: {
+    title: {
+      type: 'string',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    },
+    slug: {
+      type: 'uid',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: false
+        }
+      }
+    },
+    resume: {
+      type: 'text',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    },
+    readTime: {
+      type: 'string',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    },
+    date: {
+      type: 'date',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    },
+    category: {
+      type: 'relation',
+      relation: 'manyToOne',
+      target: 'api::category.category',
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    },
+    icon: {
+      type: 'string',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: false
+        }
+      }
+    },
+    headerImage: {
+      type: 'media',
+      multiple: false,
+      allowedTypes: ['images'],
+      pluginOptions: {
+        i18n: {
+          localized: false
+        }
+      }
+    },
+    content: {
+      type: 'richtext',
+      required: true,
+      pluginOptions: {
+        i18n: {
+          localized: true
+        }
+      }
+    },
+    featuredImage: {
+      type: 'media',
+      multiple: false,
+      allowedTypes: ['images'],
+      pluginOptions: {
+        i18n: {
+          localized: false
+        }
+      }
+    }
+  }
+} as const satisfies Schema.ContentType;
