51d8cc16422c8ff8ba0257e5f8ab3bc12b418a38
carloscncdev <carloscndev@gmail.com>
2026-06-08 21:35:13 -0600

    feat(BACK-037): fix user extension - use schema.json + whitelist fields

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,13 @@
 import type { Core } from "@strapi/strapi";
 
-const config = ({ env: _env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env: _env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: ["nickname", "avatar"],
+      },
+    },
+  },
+});
 
 export default config;
